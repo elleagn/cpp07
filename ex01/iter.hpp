6d4dc86e31da08c188330d9836b0e9651b47371a
gozon <gozon@student.42.fr>
2025-07-22 17:53:32 +0200

    Deal with const and template functions

--- ex01/iter.hpp
+++ ex01/iter.hpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 14:55:14 by gozon             #+#    #+#             */
-/*   Updated: 2025/07/22 17:07:53 by gozon            ###   ########.fr       */
+/*   Updated: 2025/07/22 17:24:58 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 #define ITER_HPP
 
 #include <cstddef>
+#include <iostream>
 
 template<typename T>
 void iter(T* array, size_t len, void (*fun)(T& element)) {
@@ -21,6 +22,22 @@ void iter(T* array, size_t len, void (*fun)(T& element)) {
     for (size_t i = 0; i < len; i++) {
         fun(array[i]);
     }
+
+}
+
+template<typename T>
+void iter(T* array, size_t len, void (*fun)(const T& element)) {
+
+    for (size_t i = 0; i < len; i++) {
+        fun(array[i]);
+    }
+
+}
+
+template<typename T>
+void printVar(const T& var) {
+
+    std::cout << var << std::endl;
 
 }
 
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/22 17:01:02 by gozon             #+#    #+#             */
-/*   Updated: 2025/07/22 17:07:20 by gozon            ###   ########.fr       */
+/*   Updated: 2025/07/22 17:29:20 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,19 @@ void addOne(int& nb) {
     nb++;
 }
 
+void printOne(const int& nb) {
+    std::cout << nb << std::endl;
+}
+
 int main(void) {
     int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-
     iter(array, 10, addOne);
-    for (size_t i = 0; i < 10; i++) {
-        std::cout << array[i] << std::endl;
-    }
+
+    std::cout << "Int array:" << std::endl;
+    iter(array, 10, printVar);
+
+    std::string strs[3] = {"Hello", "42", "blbl"};
+    std::cout << "String array:" << std::endl;
+    iter(strs, 3, printVar);
     return (0);
 }
