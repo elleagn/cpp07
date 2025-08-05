26f8638df72eaec1e3c66512b09a19566cae89e7
Gaelle Ozon <gozon@student.42.fr>
2025-08-05 09:10:07 +0200

    Remove useless function

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -17,10 +17,6 @@ void addOne(int& nb) {
     nb++;
 }
 
-void printOne(const int& nb) {
-    std::cout << nb << std::endl;
-}
-
 int main(void) {
     int array[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     iter(array, 10, addOne);
